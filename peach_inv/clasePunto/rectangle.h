6b90d621e9c16e616cb0f947a4f5909047255efa
smml1996 <michelle_muroya@hotmail.com>
2016-11-28 21:41:49 -0500

    casi terminado

--- peach_inv/clasePunto/rectangle.h
+++
@@ -1,7 +0,0 @@
-#include "polygon.h"
-class Rectangle:public Polygon{
-public:
-    Rectangle(const Point &a, const Point &b);
-    Rectangle(int x1,int y1, int x2,int y2);
-    double area();
-};
--- peach_inv/clasePunto/triangle.h
+++
@@ -1,6 +0,0 @@
-#include "polygon.h"
-class Triangle: public Polygon{
-public:
-    Triangle(Point &a, Point &b, Point &c);
-    double area();
-};
